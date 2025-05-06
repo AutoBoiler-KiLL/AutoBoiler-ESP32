f84f658214d1c4aa072985c5a47591adde0dbd1e
Leonardo Larrañaga <luis.larranaga@uabc.edu.mx>
2025-05-06 12:44:47 -0700

    checkForFactoryReset

--- include/KiLL.h
+++ include/KiLL.h
@@ -3,8 +3,14 @@
 
 #include "Memory.h"
 
-const int MINIMUM_TEMPERATURE = 25;
-const int MAXIMUM_TEMPERATURE = 45;
+const uint8_t MINIMUM_TEMPERATURE = 25;
+const uint8_t MAXIMUM_TEMPERATURE = 45;
+
+const uint8_t FACTORY_RESET_PIN = 10;
+const int FACTORY_RESET_TIMEOUT = 10000; 
+
+bool factoryButtonPressed = false;
+unsigned long factoryButtonPressedTime = 0;
 
 /// @brief Get the ESP32's unique identifier (given in the Espressif factory).
 /// This will always be the same for a given ESP32, no matter how many times it is flashed.
@@ -19,11 +25,34 @@ String espId() {
 /// @brief Resets the ESP32 to factory settings.
 /// This will clear the EEPROM and restart the ESP32.
 void resetToFactorySettings() {
-    // Reset the ESP32 to factory settings
     Serial.println("[ESP32] Resetting to factory settings...");
     clearMemory();
     delay(3000);
     ESP.restart();
+}
+
+void checkForFactoryReset() {
+    bool buttonPressed = digitalRead(FACTORY_RESET_PIN) == LOW;
+
+    // Button pressed for the first time
+    if (buttonPressed && !factoryButtonPressed) {
+        factoryButtonPressed = true;
+        factoryButtonPressedTime = millis();
+    } 
+    
+    // Button released before timeout
+    if (!buttonPressed && factoryButtonPressed) {
+        factoryButtonPressed = false;
+        if (millis() - factoryButtonPressedTime >= FACTORY_RESET_TIMEOUT) {
+            resetToFactorySettings();
+        }
+    }
+
+    // Button pressed for timeout, reset to factory settings
+    if (buttonPressed && millis() - factoryButtonPressedTime >= FACTORY_RESET_TIMEOUT) {
+        Serial.println("[ESP32] Factory reset button pressed for too long. Resetting to factory settings...");
+        resetToFactorySettings();
+    }
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,16 @@
 #include <Arduino.h>
+
 #include "WifiControl.h"
 #include "PowerControl.h"
+#include "KiLL.h"
 
 void setup () {
   Serial.begin(115200);
+  pinMode(FACTORY_RESET_PIN, INPUT_PULLUP);
   initWiFi();
 }
 
 void loop () {
   server.handleClient();
+  checkForFactoryReset();
 }
