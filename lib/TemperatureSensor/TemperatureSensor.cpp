e7b6a73041d27259938735744d2d121103ecfc89
Leonardo Larrañaga <luis.larranaga@uabc.edu.mx>
2025-06-02 17:03:16 -0700

    Stopped using computeVolts for faster processing

--- lib/TemperatureSensor/TemperatureSensor.cpp
+++ lib/TemperatureSensor/TemperatureSensor.cpp
@@ -1,4 +1,4 @@
- #include "TemperatureSensor.h"
+#include "TemperatureSensor.h"
 
 TemperatureSensor::TemperatureSensor() {
     ads = new Adafruit_ADS1115();
@@ -10,7 +10,8 @@ void TemperatureSensor::begin() {
 }
 
 double TemperatureSensor::readTemperature(uint8_t channel) {
-    double vOut = ads->computeVolts(ads->readADC_SingleEnded(channel));
+    int16_t rawADC = ads->readADC_SingleEnded(channel);
+    double vOut = (rawADC * 0.256) / 32767.0;
     double R_PT100 = (-vOut * R_FIXED) / (vOut - VCC);
     double temperature = (R_PT100 - R0) / (alpha * R0);
     return temperature;
